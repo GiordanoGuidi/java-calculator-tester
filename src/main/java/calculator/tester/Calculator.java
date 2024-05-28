0b0b7da28d67fe9c4818982ff3983ec3f056946e
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-05-28 16:32:10 +0200

    added other tests

--- src/main/java/calculator/tester/Calculator.java
+++ src/main/java/calculator/tester/Calculator.java
@@ -5,24 +5,27 @@ public class Calculator {
     //Metodi
     //addizione
     public float add(float num1,float num2){
+        if (Float.isNaN(num1) || Float.isNaN(num2)){
+            throw new NumberFormatException("One or both parameters are not valid float numbers");
+        }
         return num1 + num2;
     }
     //sottrazione
     public float substract(float num1,float num2){
+        if (Float.isNaN(num1) || Float.isNaN(num2)){
+            throw new NumberFormatException("One or both parameters are not valid float numbers");
+        }
         return num1 - num2;
     }
     //Divisione
     public float divide(float num1, float num2){
-        if (num2 == 0){
+        if (num2 == 0 || Float.isNaN(num1) || Float.isNaN(num2)){
             throw new ArithmeticException("The divisor must not be zero");
         }
         return num1 / num2;
     }
     //Moltiplicazione
     public float multiply(float num1, float num2){
-        if (Float.isNaN(num1) || Float.isNaN(num2)){
-            throw new NumberFormatException("One or both parameters are not valid float numbers");
-        }
         return num1 * num2;
     }
 }
--- src/test/java/calculator/tester/CalculatorTest.java
+++ src/test/java/calculator/tester/CalculatorTest.java
@@ -9,33 +9,37 @@ class CalculatorTest {
     @Test
     void add() {
         Calculator calculator = new Calculator();
-        assertEquals(5.0f,calculator.add(2.0f,3.0f));
-        assertEquals(2.6f,calculator.add(1.2f,1.4f));
-        assertEquals(0.0f,calculator.add(1.0f,-1.0f));
+        assertEquals(5.0f,calculator.add(2,3));
+        assertEquals(2.6f,calculator.add(2.6f,0));
+        assertEquals(0.0f,calculator.add(1,-1));
+        assertNotEquals(2.3f,calculator.add(1.5f,1));
     }
 
     @Test
     void substract() {
         Calculator calculator = new Calculator();
-        assertEquals(5.0f,calculator.substract(7.0f,2.0f));
-        assertEquals(8.8f,calculator.substract(10.0f,1.2f));
-        assertEquals(-2.0f,calculator.substract(10.0f,12.0f));
+        assertEquals(5,calculator.substract(7,2));
+        assertEquals(10,calculator.substract(10,0));
+        assertEquals(-2,calculator.substract(10,12));
+        assertNotEquals(-2,calculator.substract(11,12));
     }
 
     @Test
     void divide() {
         Calculator calculator = new Calculator();
-        assertEquals(4.0f,calculator.divide(16.0f,4.0f));
-        assertEquals(5.0f,calculator.divide(50.0f,10.0f));
-        assertEquals(1.0f,calculator.divide(50.0f,50.0f));
+        assertEquals(4,calculator.divide(16.0f,4));
+        assertEquals(5,calculator.divide(50,10));
+        assertEquals(1,calculator.divide(50,50));
+        assertNotEquals(1,calculator.divide(50,5));
+        assertThrows(ArithmeticException.class,()-> calculator.divide(2f,0f));
     }
 
     @Test
     void multiply() {
         Calculator calculator = new Calculator();
-        assertEquals(8,calculator.multiply(2.0f,4.0f));
-        assertEquals(5,calculator.multiply(2.5f,2.0f));
-        assertEquals(5,calculator.multiply(2.5f,2.0f));
-
+        assertEquals(8,calculator.multiply(2,4));
+        assertEquals(5,calculator.multiply(2.5f,2));
+        assertEquals(5,calculator.multiply(2.5f,2));
+        assertNotEquals(8,calculator.multiply(2.5f,2));
     }
 }
