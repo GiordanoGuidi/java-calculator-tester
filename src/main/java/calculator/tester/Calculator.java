9657807dc42a50dbcc018bcb30479d45f5756dec
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-05-28 15:05:18 +0200

    Initial commit

---
+++ src/main/java/calculator/tester/Calculator.java
@@ -0,0 +1,28 @@
+package calculator.tester;
+
+public class Calculator {
+
+    //Metodi
+    //addizione
+    public float add(float num1,float num2){
+        return num1 + num2;
+    }
+    //sottrazione
+    public float substract(float num1,float num2){
+        return num1 - num2;
+    }
+    //Divisione
+    public float divide(float num1, float num2){
+        if (num2 == 0){
+            throw new ArithmeticException("The divisor must not be zero");
+        }
+        return num1 / num2;
+    }
+    //Moltiplicazione
+    public float multiply(float num1, float num2){
+        if (Float.isNaN(num1) || Float.isNaN(num2)){
+            throw new NumberFormatException("One or both parameters are not valid float numbers");
+        }
+        return num1 * num2;
+    }
+}
---
+++ src/test/java/calculator/tester/CalculatorTest.java
@@ -0,0 +1,24 @@
+package calculator.tester;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void substract() {
+    }
+
+    @Test
+    void divide() {
+    }
+
+    @Test
+    void multiply() {
+    }
+}
